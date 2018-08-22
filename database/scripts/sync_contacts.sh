1b8c103d1abfe69c8dd84db62c32ad68f3e34249
Mike Minckler <mikeminckler@gmail.com>
2018-08-22 10:46:25 -0700

    updated sync script

--- database/scripts/sync_contacts.sh
+++ database/scripts/sync_contacts.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-mysqldump -u myschool_ro -pNewcastle44 -h cirdan.brentwood.bc.ca MYSCHOOL_LOCAL contacts | mysql -u root -pNewcastle22 noto_brentwood
-mysql -u root -pNewcastle22 noto_brentwood -e "ALTER TABLE noto_brentwood.contacts CHANGE COLUMN user_id id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT"
+mysqldump -u noto_user -pNewcastle22 -h moodle.brentwood.bc.ca myschool_brentwood contacts | mysql -u noto_user -pNewcastle22 noto_brentwood
