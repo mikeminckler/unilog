6aadec92a834fc005f86d43ab697e9ad3a0e48cf
Mike Minckler <mikeminckler@gmail.com>
2018-09-14 18:47:24 +0000

    updated sync script

--- database/scripts/sync_contacts.sh
+++ database/scripts/sync_contacts.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 mysqldump -u noto_user -pNewcastle22 -h moodle.brentwood.bc.ca myschool_brentwood contacts | mysql -u noto_user -pNewcastle22 noto_brentwood
+
+#mysql -u noto_user -pNewcastle22 -h moodle.brentwood.bc.ca noto_brentwood -e 'ALTER TABLE noto_brentwood.contacts CHANGE COLUMN id id_old BINARY(16) NOT NULL , CHANGE COLUMN user_id id MEDIUMINT(8) UNSIGNED NOT NULL;'
+
+mysql -u noto_user -pNewcastle22 -h moodle.brentwood.bc.ca noto_brentwood -e 'ALTER TABLE noto_brentwood.contacts  CHANGE COLUMN `id` `id_old` BINARY(16) NOT NULL , CHANGE COLUMN `user_id` `id` MEDIUMINT(8) UNSIGNED NOT NULL;'
